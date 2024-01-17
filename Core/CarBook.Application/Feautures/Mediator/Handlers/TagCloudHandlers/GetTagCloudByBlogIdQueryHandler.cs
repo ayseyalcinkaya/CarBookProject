3d0f7296a89a45d0befef860e56fa4373c7d5d0b
aysey <aysey@AYSEPC>
2024-01-17 14:18:06 +0300

    ...

---
+++ Core/CarBook.Application/Feautures/Mediator/Handlers/TagCloudHandlers/GetTagCloudByBlogIdQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using CarBook.Application.Feautures.Mediator.Queries.TagCloudQueries;
+using CarBook.Application.Feautures.Mediator.Results.TagCloudResults;
+using CarBook.Application.Interfaces.TagCloudInterfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Feautures.Mediator.Handlers.TagCloudHandlers
+{
+    public class GetTagCloudByBlogIdQueryHandler : IRequestHandler<GetTagCloudByBlogIdQuery, List<GetTagCloudByBlogIdQueryResult>>
+    {
+        private readonly ITagCloudRepository _repository;
+
+        public GetTagCloudByBlogIdQueryHandler(ITagCloudRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<GetTagCloudByBlogIdQueryResult>> Handle(GetTagCloudByBlogIdQuery request, CancellationToken cancellationToken)
+        {
+            var values = _repository.GetTagCloudsByBlogID(request.Id);
+            return values.Select(x => new GetTagCloudByBlogIdQueryResult
+            {
+
+                TagCloudID = x.TagCloudID,
+                Title = x.Title,
+                BlogID = x.BlogID,
+
+            }).ToList();
+        }
+    }
+}
