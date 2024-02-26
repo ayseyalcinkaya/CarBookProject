555ef599932ccc2a28066124e7cbd3587d1ea584
ayseyalcinkaya <aysey@AYSEPC>
2024-02-26 20:20:46 +0300

    Update and Remove

--- Presentation/CarBook.WebApi/Controllers/FeaturesController.cs
+++ Presentation/CarBook.WebApi/Controllers/FeaturesController.cs
@@ -39,7 +39,7 @@ namespace CarBook.WebApi.Controllers
 
         }
 
-        [HttpDelete]
+        [HttpDelete("{id}")]
         public async Task<IActionResult> RemoveFeature(int id)
         {
             await _mediator.Send(new RemoveFeatureCommand(id));
